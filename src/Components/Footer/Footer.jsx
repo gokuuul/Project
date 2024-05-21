8404126ca750c5d3a93969c219b906882c2ebf72
gokuuul <113451974+gokuuul@users.noreply.github.com>
2024-05-21 20:58:29 +0530

    Add files via upload

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <hr />
+      <p>Group_09</p>
+    </div>
+  )
+}
+
+export default Footer
